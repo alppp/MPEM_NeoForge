aab51e09e1295bf49c0f0287478d0b04cd704050
Dan <turcu.dan991@gmail.com>
2025-07-29 19:34:51 +0200

    fix morejs compatibility

--- src/main/java/net/shuyanmc/mpem/mixin/EnchantmentMenuMixin.java
+++ src/main/java/net/shuyanmc/mpem/mixin/EnchantmentMenuMixin.java
@@ -7,9 +7,9 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(EnchantmentMenu.class)
+@Mixin(value = EnchantmentMenu.class, priority = 500)
 public class EnchantmentMenuMixin {
-    @Redirect(method = "slotsChanged", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;isEnchantable()Z"))
+    @Redirect(method = "slotsChanged", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;isEnchantable()Z"), require = 0)
     public boolean mpem$slotChanged$InvokeEnchantableEvent(ItemStack instance) {
         try {
             // Enhanced enchantability check - allow single-stack items with durability
